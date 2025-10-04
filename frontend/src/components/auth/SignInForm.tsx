6c08521a5164614b80eb54269b0e939a9529e669
Rodrigo Garcia <dev.raxielh@gmail.com>
2025-10-03 22:19:51 -0500

    init

--- frontend/src/components/auth/SignInForm.tsx
+++ frontend/src/components/auth/SignInForm.tsx
@@ -35,7 +35,7 @@ export default function SignInForm() {
         <div>
           <div className="mb-5 sm:mb-8">
             <h1 className="mb-2 font-semibold text-gray-800 text-title-sm dark:text-white/90 sm:text-title-md">
-              Asistente de investigadores Minciencias
+              Asistente de grupos de investigación Minciencias
             </h1>
             <p className="text-sm text-gray-500 dark:text-gray-400">
               Ingrese su usuario y contraseña
